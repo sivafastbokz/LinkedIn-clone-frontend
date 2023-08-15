9f84c3adf4f2c05480133fa8dd01c1d563c392f6
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-08-15 19:32:29 +0530

    some updates pending

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import Logo from '../reUseComponent/Logo';
+import { useNavigate } from 'react-router-dom';
 import { Logout } from '@mui/icons-material';
 import { Search } from '@mui/icons-material';
+import { TextField } from '@mui/material';
+import {InputAdornment} from '@mui/material';
 import './header.css'
 
 function Header(){
@@ -16,8 +18,21 @@ function Header(){
         <>
         <div className='header'>
         <Logo className='logo-feed'/> 
-        <input className='input-search' placeholder='     Search' type='text'></input>
-        <Search className='search-btn'/>
+        {/* <input className='input-search' placeholder='     Search' type='text'></input>
+        <Search className='search-btn'/> */}
+        <TextField 
+        placeholder='Search' 
+        className='input-search'
+        InputProps={{
+            startAdornment:(
+                <InputAdornment position='start'>
+                   <Search className='search-btn'/>
+                </InputAdornment>
+            )
+        }}
+        variant='outlined'
+        size='small'
+        />
         <Logout className='logout-btn' onClick={logOut} titleAccess='logout'/>
         </div>
         </>
