44985097a3e1243f5bd4c3a0144e393c5a7316fc
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-09-04 13:30:36 +0530

    updated

--- src/helper/axiosClient.js
+++ src/helper/axiosClient.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import ServerConfigs from '../configs/serverConfig';
 
+const isDevelopment =process.env.NODE_ENV === 'development'
+const baseURL = isDevelopment ? ServerConfigs : 'https://linkedin-clone-backend-hl4v.onrender.com'
+
 const axiosClient = axios.create({
-    baseURL:ServerConfigs,
+    baseURL,
     headers:{
         'Accept': 'application/json',
         'Content-Type': 'application/json',
